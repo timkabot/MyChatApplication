6eec5c6bac0b44230ecc984f32d2e731d859a046
Timur Borgalinov <borgalinov@gmail.com>
2020-01-09 13:15:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mydemochat/MainActivity.kt
@@ -0,0 +1,29 @@
+ package com.example.mydemochat
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+
+ class MainActivity : AppCompatActivity() {
+     private var mAuth: FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+     override fun onStart() {
+         super.onStart()
+         // Check if user is signed in (non-null) and update UI accordingly.
+         val currentUser = mAuth!!.currentUser
+         //updateUI(currentUser)
+
+         if(currentUser == null){
+             val startIntent = Intent(this, StartActivity::class.java)
+             startActivity(startIntent)
+             //finish()
+         }
+     }
+}
